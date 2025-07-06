37947ae4d4fd24fa1348b11de49733a7f3dbde51
khh-Niloy <niloy.dev.101@gmail.com>
2025-07-06 21:34:09 +0600

    update: update post api endpoint

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import fitz  # PyMuPDF
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # allow all for now (you can restrict later)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
-    return {"message": "PDF Parser Service is running"}
+    return {"message": "Career Copilot : PDF Parser Service is running at 9000"}
 
-@app.post("/extract")
+@app.post("/api/pdf/extract")
 async def extract_pdf(pdf: UploadFile = File(...)):
+
+    if pdf.content_type != "application/pdf":
+        return {"error": "Only PDFs are accepted"}
+
     content = await pdf.read()
     doc = fitz.open(stream=content, filetype="pdf")
 
@@ -21,7 +35,6 @@ async def extract_pdf(pdf: UploadFile = File(...)):
         for link in page.get_links():
             if "uri" in link:
                 url = link["uri"]
-                # Simple naming heuristic example:
                 if "github.com" in url:
                     name = "GitHub"
                 elif "linkedin.com" in url:
